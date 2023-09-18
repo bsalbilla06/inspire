76824d01b9a86e0678f068abec54c1ee662f364a
bsalbilla06 <117684557+bsalbilla06@users.noreply.github.com>
2023-09-18 14:07:18 +0000

    json parsing

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
   "fmt"
+  "encoding/json"
 )
 
+type Quote struct {
+	Words string	`json:"q"`
+	Author string	`json:"a"`
+}
+
 func main() {
-  fmt.Println("Hello, World!")
+	var quote1 Quote
+
+	Data := []byte(`{
+		"q": "A wise man once said nothing at all",
+		"a": "Drake"
+	}`)
+
+	err := json.Unmarshal(Data, &quote1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%v said \"%v\"\n", quote1.Author, quote1.Words)
 }
